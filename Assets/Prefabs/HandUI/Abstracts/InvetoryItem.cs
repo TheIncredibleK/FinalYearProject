da684cb203135964e6fab7d11b1276b92a0e3aeb
TheIncredibleK <keith.kny@gmail.com>
2017-03-03 17:32:47 +0000

    Adding correct MainUI prefab

---
+++ Assets/Prefabs/HandUI/Abstracts/InvetoryItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InvetoryItem : MonoBehaviour {
+	public int amount;
+	public float value;
+
+
+
+	void Remove(int toRemove) {
+		this.amount -= toRemove;
+	}
+
+	void Add(int toRemove) {
+		this.amount += toRemove;
+	}
+}
